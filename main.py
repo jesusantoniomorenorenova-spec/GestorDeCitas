4fb0f9185aab05d7f33f042cbedff9d1cea8011e
tono <jesusantoniomorenorenova@gmail.com>
2026-03-10 11:59:49 -0600

    loginAgregado

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import sys
-from PyQt6.QtWidgets import QApplication
-from controllers.dashboard_controller import DashboardWindow
-
+from pathlib import Path
 from PyQt6 import QtWidgets , uic
-from controllers.login_controller import LoginController
 from PyQt6.QtCore import pyqtSignal
-from PyQt6.QtGui import QPalette
-from PyQt6.QtWidgets import QMainWindow
+from controllers.login_controller import LoginController
+from controllers.dashboard_controller import DashboardWindow
+from PyQt6.QtWidgets import QApplication
 from PyQt6 import uic
+
+
+BASE_DIR = Path(__file__).resolve().parent
+UI_DIR = BASE_DIR / "ui"
 
 
 #pip install pyqt6-tools
@@ -17,41 +19,27 @@ class Login(QtWidgets.QDialog):
 
     def __init__(self):
         super().__init__()
-        uic.loadUi("./ui/login.ui", self)
+        uic.loadUi(str(UI_DIR / "login.ui"),self)
         self.controller = LoginController(self,self)
-        self.apply_theme()
-
-    def apply_theme(self):
-        is_dark = self.palette().color(QPalette.ColorRole.Window).lightness() <128
-        print(f"  dark: {is_dark}")
-
-class Sells(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi("./ui/dashboard.ui", self)
-
 
 class AppManager:
     def __init__(self):
         self.login_window=Login()
-        self.sell_window = None
-        self.sell_window = Sells()
+        self.main_Window = DashboardWindow()
         self.login_window.login_successful.connect(self.show_main_window)
 
         self.login_window.show()
+
     def show_main_window(self):
-        self.sell_window.show()#mostrar ventana de muestra
+        self.main_Window.show()#mostrar ventana de muestra
         self.login_window.close()#ceramos ventana
 
-app = QtWidgets.QApplication(sys.argv)
-manager = AppManager()
-sys.exit(app.exec())
 
 
 def main():
     app = QApplication(sys.argv)
-    w = DashboardWindow()
-    w.show()
+
+    manager = AppManager()
     sys.exit(app.exec())
 
 if __name__ == "__main__":
@@ -59,7 +47,7 @@ if __name__ == "__main__":
 
 
 
-
+#jesus antonio moreno
 
 
 
